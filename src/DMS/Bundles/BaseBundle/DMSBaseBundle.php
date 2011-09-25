b79ceb0d57a96f0dea8ffebb07692fb5ec9848dd
Rafael Dohms <rdohms@gmail.com>
2011-09-25 09:47:22 -0300

    Base changes and addition of DMSBaseBundle, responsible for bootstraping

---
+++ src/DMS/Bundles/BaseBundle/DMSBaseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DMS\Bundles\BaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DMSBaseBundle extends Bundle
+{
+}
--- web/app.php
+++ web/app.php
@@ -4,9 +4,15 @@ require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
+// Define application environment
+defined('APP_ENV')
+    || define('APP_ENV', (getenv('APP_ENV') ? getenv('APP_ENV') : 'prod'));
+
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('prod', false);
+setlocale(LC_ALL, 'pt_BR');
+
+$kernel = new AppKernel(APP_ENV, (APP_ENV == 'dev')? true:false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();
