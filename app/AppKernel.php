0f17f2f135e13b80815fc69a33191d2c7425326a
Rafael Dohms <rdohms@gmail.com>
2011-09-25 10:37:10 -0300

    Adding skeleton for AuthBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new DMS\Bundles\BaseBundle\DMSBaseBundle(),
+            new FTC\Bundles\AuthBundle\FTCAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/FTC/Bundles/AuthBundle/FTCAuthBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FTC\Bundles\AuthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FTCAuthBundle extends Bundle
+{
+}
