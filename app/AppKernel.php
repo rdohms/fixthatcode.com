1e8df1a17191bde2eeaf63b65486a5de681259a8
Rafael Dohms <rdohms@gmail.com>
2011-09-30 00:31:44 -0300

    Moving DMS Bundles into DMS repository and adding it via vendors Fixing FTCExtension config Removing AcmeBundle routes Pointing to Sf 2.0.3 to avoid DEV issues

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,11 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new DMS\Bundles\BaseBundle\DMSBaseBundle(),
             new FTC\Bundles\AuthBundle\FTCAuthBundle(),
+            new DMS\Bundles\LauncherBundle\DMSLauncherBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            // $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
--- src/DMS/Bundles/BaseBundle/DMSBaseBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DMS\Bundles\BaseBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DMSBaseBundle extends Bundle
-{
-}
