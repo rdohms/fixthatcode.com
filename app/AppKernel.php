c5ccc0b57882ec92287cd654809ccde769be9ed5
Rafael Dohms <rdohms@gmail.com>
2013-05-20 18:54:51 +0200

    Commiting abandoned code.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Ornicar\GravatarBundle\OrnicarGravatarBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Shtumi\UsefulBundle\ShtumiUsefulBundle(),
             //new DMS\Bundle\LauncherBundle\DMSLauncherBundle(),
 
             new FTC\Bundle\WebBundle\FTCWebBundle(),
